3aad72884a2b05654b4e910f0fde6c1ba7dc56e6
donghyun <imkim1893@naver.com>
2022-02-09 15:20:34 +0000

    수정완료

--- main.dart
+++ main.dart
@@ -7,13 +7,16 @@ import './util/PuppeteerUtil.dart';
 final p = PuppeteerUtil();
 final delay = Duration(milliseconds: 100);
 final timeout = Duration(seconds: 20);
+final List<String> listToIncludeAlways = const ["flutter"];
+final List<String> listToInclude = const ["취미/자기개발", "앱 개발"];
+final List<String> listToExclude = const ["초등학생", "중학생", "고등학생", "20대"];
 
 void main() async {
   Map localData = FileUtil.readJsonFile("./local.json");
   p.openBrowser(() async {
     while (true) {
       await login(localData);
-      await p.deleteRequests();
+      await deleteRequests();
 
       double waitMinutes = (5 + Random().nextInt(5)).toDouble();
       await p.wait(waitMinutes * 60 * 1000);
@@ -45,4 +48,58 @@ Future<bool> isLoginSuccess() async {
 Future<bool> checkLoginFail() async {
   return await p.evaluate(
       r"((document.querySelector('.invalid-feedback')?.innerText ?? '').includes('입력해주세요')) || ((document.querySelector('.form-text.text-invalfid')?.innerText ??'').includes('입력해주세요'))");
+}
+
+Future<void> deleteRequests() async {
+  print("deleteRequests 시작");
+  await p.goto('https://soomgo.com/requests/received');
+
+  List<ElementHandle> tagList =
+      await p.$$('.request-list > li > .request-item');
+  if (tagList.isEmpty) {
+    print("요청이 없습니다.");
+    return;
+  }
+
+  for (var tag in tagList) {
+    var messageTag = await p.$('.quote > span.message', tag: tag);
+    String message = await p.tagHtml(messageTag);
+
+    if (!isValidRequest(message)) {
+      p.click('.quote-btn.del',tag:tag);
+      p.click('.sv-col-small-button-bw.sv__btn-close');
+      p.click('.swal2-confirm.btn');
+
+      print("삭제할 tagText : " + message);
+    } else {
+      print("내가 좋하하는 tagText : " + message);
+    }
+  }
+}
+
+bool isValidRequest(String message) {
+  bool isValid = true;
+  //이 키워드가 없으면, !isValid
+  for (String toInclude in listToInclude) {
+    if (!message.contains(toInclude)) {
+      isValid = false;
+      break;
+    }
+  }
+  //이 키워드가 있으면, !isValid
+  for (String toExclude in listToExclude) {
+    if (message.contains(toExclude)) {
+      isValid = false;
+      break;
+    }
+  }
+  //이 키워드가 있으면, 무조건 isValid
+  for (String toIncludeAlways in listToIncludeAlways) {
+    if (message.toLowerCase().contains(toIncludeAlways.toLowerCase())) {
+      isValid = true;
+      break;
+    }
+  }
+
+  return isValid;
 }
--- util/PuppeteerUtil.dart
+++ util/PuppeteerUtil.dart
@@ -13,9 +13,6 @@ class PuppeteerUtil {
 
   final defaultTimeout = Duration(seconds: 10);
 
-  final List<String> listToInclude = const ["취미/자기개발", "앱 개발"];
-  final List<String> listToExclude = const ["초등학생", "중학생", "고등학생", "20대"];
-
   Future<void> openBrowser(Future<void> Function() function) async {
     //open
     browser = await puppeteer.launch(
@@ -42,63 +39,6 @@ class PuppeteerUtil {
     } catch (e) {}
   }
 
-  Future<void> deleteRequests() async {
-    print("deleteRequests 시작");
-    await goto('https://soomgo.com/requests/received');
-
-    List<ElementHandle> tagList =
-        await tab.$$('.request-list > li > .request-item');
-    if (tagList.isEmpty) {
-      print("요청이 없습니다.");
-      return;
-    }
-    print("요청이 있습니다.");
-
-    for (var tag in tagList) {
-      var messageTag = await tag.$('.quote > span.message');
-      String message = await tagHtml(messageTag);
-
-      bool validRequest = true;
-      //포함할 request
-      for (String toInclude in listToInclude) {
-        if (!message.contains(toInclude)) {
-          validRequest = false;
-          break;
-        }
-      }
-      //제외할 request
-      for (String toExclude in listToExclude) {
-        if (message.contains(toExclude)) {
-          validRequest = false;
-          break;
-        }
-      }
-
-      if (!validRequest) {
-        var deleteTag = await tag.$('.quote-btn.del');
-        await deleteTag.click();
-
-        try {
-          await tab.waitForSelector('.sv-col-small-button-bw.sv__btn-close');
-          var closeTag = await tab.$('.sv-col-small-button-bw.sv__btn-close');
-          await closeTag.click();
-        } catch (e) {}
-
-        // FileUtil.writeFile("body.html", await bodyHtml(tab));
-
-        try {
-          await tab.waitForSelector('.swal2-confirm.btn');
-          var dialogTag = await tab.$('.swal2-confirm.btn');
-          await dialogTag.click();
-        } catch (e) {}
-
-        print("삭제할 tagText : " + message);
-      } else {
-        print("내가 좋하하는 tagText : " + message);
-      }
-    }
-  }
-
   Future<void> goto(String url) async {
     await tab.goto(url, wait: Until.networkIdle);
   }
@@ -106,7 +46,7 @@ class PuppeteerUtil {
   Future<String> bodyHtml() async {
     return await tab.content ?? "";
   }
-  
+
   Future<String> tagHtml(ElementHandle tag) async {
     return await evaluate(r'el => el.textContent', args: [tag]);
   }
@@ -114,7 +54,6 @@ class PuppeteerUtil {
   Future<dynamic> evaluate(String pageFunction, {List? args}) async {
     return await tab.evaluate(pageFunction, args: args);
   }
-
 
   Future<void> type(String selector, String content, {Duration? delay}) async {
     await tab.type(selector, content, delay: delay);
@@ -136,5 +75,31 @@ class PuppeteerUtil {
             setTimeout(resolve, $millseconds)
       });
   }''');
+  }
+
+  Future<List<ElementHandle>> $$(String selector, {ElementHandle? tag}) async {
+    if (tag != null) {
+      return await tag.$$(selector);
+    } else {
+      return await tab.$$(selector);
+    }
+  }
+
+  Future<ElementHandle> $(String selector, {ElementHandle? tag}) async {
+    if (tag != null) {
+      return await tag.$(selector);
+    } else {
+      return await tab.$(selector);
+    }
+  }
+
+  Future<void> click(String selector, {ElementHandle? tag}) async {
+    try {
+      if (tag == null) {
+        await tab.waitForSelector(selector);
+      }
+      var tagToClick = await $(selector, tag: tag);
+      await tagToClick.click();
+    } catch (e) {}
   }
 }
